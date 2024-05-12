b52a2311b315f70e085cfcbb0dd8affa1698ffd0
olavi <olavi@hahtomaa.fi>
2024-05-12 15:18:07 +0300

    Unit

--- src/main/java/org/main/uneton/commands/Guide.java
+++ src/main/java/org/main/uneton/commands/Guide.java
@@ -52,7 +52,7 @@ public class Guide implements CommandExecutor {
                     player.sendMessage(b + "> /ignore <player> " + f + " | Ignore a player.");
                     player.sendMessage(b + "> /ignorelist <player> " + f + " | Checks all ignored players.");
                     player.sendMessage(b + "> /unignore <player> " + f + " | Un ignores a player.");
-                    player.sendMessage(b + "> /trashcan " + f + " | Items that you want to deleted permanently!");
+                    player.sendMessage(b + "> /trashcan " + f + " | Put your stuff in the menu and close it!");
                     player.sendMessage(ChatColor.GRAY.toString() + ChatColor.BOLD + "-------------------------------------");
                     break;
                 }
