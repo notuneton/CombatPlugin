37272bb6f3d7abe9d17021818633ba2e860c8027
olavi <olavi@hahtomaa.fi>
2024-06-08 16:25:53 +0300

    Unit

--- src/main/java/org/main/uneton/Combat.java
+++ src/main/java/org/main/uneton/Combat.java
@@ -72,6 +72,7 @@ public class Combat extends JavaPlugin implements Listener {
         // listeners
         Bukkit.getPluginManager().registerEvents(new Listeners(this), this);
         Bukkit.getPluginManager().registerEvents(new MessageHolder(), this);
+        Bukkit.getPluginManager().registerEvents(new Deaths(), this);
 
         getCommand("freeze").setExecutor(new Freeze());
         Bukkit.getPluginManager().registerEvents(new FreezeListener(), this);
