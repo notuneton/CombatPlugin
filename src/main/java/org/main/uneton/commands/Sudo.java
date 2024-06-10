70db56096f178815704a2baba3a7b8efe3bd91e3
olavi <olavi@hahtomaa.fi>
2024-06-10 22:34:25 +0300

    Unit

--- src/main/java/org/main/uneton/commands/Sudo.java
+++ src/main/java/org/main/uneton/commands/Sudo.java
@@ -13,7 +13,7 @@ public class Sudo implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         Player player = (Player) sender;
-        if(args.length == 0) {
+        if (args.length == 0) {
             player.sendMessage(ChatColor.RED + "> /sudo <target> <command>");
             return true;
         }
