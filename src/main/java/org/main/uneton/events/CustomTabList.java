0010b4a9b299ffff72feeca6834756d63e92af7e
olavi <olavi@hahtomaa.fi>
2024-06-18 21:01:48 +0300

    Unit

--- src/main/java/org/main/uneton/events/CustomTabList.java
+++ src/main/java/org/main/uneton/events/CustomTabList.java
@@ -8,7 +8,7 @@ public class CustomTabList {
 
     public static void updateTabList() {
         String headerTemplate = "\n &x&4&D&9&8&F&B[&x&6&6&A&6&F&B[ &x&F&F&F&F&F&F&lQ&x&F&F&F&F&F&F&lu&x&F&F&F&F&F&F&lo&x&F&F&F&F&F&F&ll&x&F&F&F&F&F&F&ll&x&F&F&F&F&F&F&le&x&F&F&F&F&F&F&le&x&F&F&F&F&F&F&lt&x&F&F&F&F&F&F&l.&x&F&F&F&F&F&F&lc&x&F&F&F&F&F&F&lo &x&4&D&9&8&F&B]&x&6&6&A&6&F&B] \n";
-        String footerTemplate = "\n &7Olet palvelimella&8: &x&8&B&B&D&F&F&lS&x&7&0&A&9&E&9&lu&x&5&5&9&4&D&2&lr&x&3&A&8&0&B&C&lv&x&2&F&7&A&B&7&li&x&3&2&8&3&C&4&lv&x&3&6&8&B&D&1&la&x&3&9&9&4&D&E&ll \n";
+        String footerTemplate = "\n &7Olet palvelimella&8: &x&B&7&F&B&6&B&lS&x&A&8&F&C&8&3&lu&x&9&A&F&D&9&C&lr&x&8&B&F&E&B&4&lv&x&8&6&F&C&B&0&li&x&8&9&F&6&9&0&lv&x&8&C&E&F&7&1&la&x&8&F&E&9&5&1&ll \n";
 
         String header = headerTemplate.replace("&D", "&d").replace("&A", "&a").replace("&B", "&b")
                 .replace("&F", "&f").replace("&Q", "&q").replace("&S", "&s");
@@ -21,4 +21,3 @@ public class CustomTabList {
         }
     }
 }
-
