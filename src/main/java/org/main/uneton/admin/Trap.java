b5198b31e9f71206b08917f2e54063e28d34ed2e
olavi <olavi@hahtomaa.fi>
2024-06-21 13:20:13 +0300

    Unit

--- src/main/java/org/main/uneton/admin/Trap.java
+++ src/main/java/org/main/uneton/admin/Trap.java
@@ -5,6 +5,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import org.main.uneton.Combat;
@@ -42,7 +44,7 @@ public class Trap implements CommandExecutor {
         return true;
     }
 
-    public static void spawnTrap(Player player) {
+    public void spawnTrap(Player player) {
         Location loc = player.getLocation();
         Location bottomCorner = loc.clone().add(0, 0, 0);
         player.setGameMode(GameMode.ADVENTURE);
@@ -56,10 +58,13 @@ public class Trap implements CommandExecutor {
                 }
             }
         }
+
         Location middleLocation = bottomCorner.clone().add(2, 2, 2);
         player.teleport(middleLocation);
-        player.sendTitle(ChatColor.RED.toString() + ChatColor.BOLD + "Trapped!",
-                ChatColor.RED + "You have been trapped in a box!", 10, 70, 20);
+        player.sendTitle(ChatColor.RED.toString() + ChatColor.BOLD + "Trapped!", ChatColor.RED + "You have been trapped in a box!", 10, 70, 20);
+        applyBlindness(player, 10, 1); // Applies blindness for 10 seconds at level 1
+
+
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -84,5 +89,14 @@ public class Trap implements CommandExecutor {
             }
         }
     }
+
+    public void applyBlindness(Player player, int duration, int amplifier) {
+        // Create a new PotionEffect instance for blindness
+        PotionEffect blindnessEffect = new PotionEffect(PotionEffectType.BLINDNESS, (int) (duration * 20L), amplifier);
+
+        // Apply the effect to the player
+        player.addPotionEffect(blindnessEffect);
+    }
+
 }
 
