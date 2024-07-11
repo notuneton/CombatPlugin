898a8092daf835b5592ffd6027b1160707cff245
olavi <olavi@hahtomaa.fi>
2024-07-11 18:18:34 +0300

    Unit

--- src/main/java/org/main/uneton/combatlogger/Spawn.java
+++ src/main/java/org/main/uneton/combatlogger/Spawn.java
@@ -58,7 +58,8 @@ public class Spawn implements CommandExecutor {
 
     private boolean teleportPlayer(Player player, Location initialLocation) {
         if (combat_tagged.containsKey(player)) {
-            player.sendMessage(ChatColor.RED + "You cannot do this in combat.");
+            String warn = ColorUtils.colorize("&x&2&C&0&9&1&6&l>&x&5&C&1&2&2&F&l>&x&C&7&5&3&4&7&l> &x&2&E&2&E&2&E&l- ");
+            player.sendMessage(warn + ChatColor.RED + "You cannot do this in combat.");
             return false;
         }
 
