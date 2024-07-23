7c6fec3f6fc8c6b5ac647ed439c9c09b647a05b5
olavi <olavi@hahtomaa.fi>
2024-07-23 14:33:26 +0300

    Unit

--- src/main/java/org/main/uneton/magicStick/MagicStick.java
+++ src/main/java/org/main/uneton/magicStick/MagicStick.java
@@ -24,7 +24,7 @@ public class MagicStick implements CommandExecutor {
         }
 
         if (args.length == 0) {
-            String usage = ColorUtils.colorize("&x&2&C&0&9&1&6&l>&x&5&C&1&2&2&F&l>&x&C&7&5&3&4&7&l> &x&2&E&2&E&2&E&l- &7Usage: &x&A&B&A&B&A&B/&x&A&B&A&B&A&Bm&x&A&B&A&B&A&Ba&x&A&B&A&B&A&Bg&x&A&B&A&B&A&Bi&x&A&B&A&B&A&Bc&x&A&B&A&B&A&Bs&x&A&B&A&B&A&Bt&x&A&B&A&B&A&Bi&x&A&B&A&B&A&Bc&x&A&B&A&B&A&Bk &x&A&B&A&B&A&B<&x&A&B&A&B&A&Bp&x&A&B&A&B&A&Bl&x&A&B&A&B&A&Ba&x&A&B&A&B&A&By&x&A&B&A&B&A&Be&x&A&B&A&B&A&Br&x&A&B&A&B&A&B>");
+            String usage = ColorUtils.colorize("&x&2&C&0&9&1&6&l>&x&5&C&1&2&2&F&l>&x&C&7&5&3&4&7&l> &x&2&E&2&E&2&E&l- &7&x&A&B&A&B&A&B/&x&A&B&A&B&A&Bm&x&A&B&A&B&A&Ba&x&A&B&A&B&A&Bg&x&A&B&A&B&A&Bi&x&A&B&A&B&A&Bc&x&A&B&A&B&A&Bs&x&A&B&A&B&A&Bt&x&A&B&A&B&A&Bi&x&A&B&A&B&A&Bc&x&A&B&A&B&A&Bk &x&A&B&A&B&A&B<&x&A&B&A&B&A&Bp&x&A&B&A&B&A&Bl&x&A&B&A&B&A&Ba&x&A&B&A&B&A&By&x&A&B&A&B&A&Be&x&A&B&A&B&A&Br&x&A&B&A&B&A&B>");
             player.sendMessage(usage);
             return true;
         }
---
+++ src/main/java/org/main/uneton/magicStick/MagicStickEvent.java
@@ -0,0 +1,13 @@
+package org.main.uneton.magicStick;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class MagicStickEvent implements Listener {
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event) {
+
+    }
+}
