ac160b86b7fbca0dffdaa30ed7cd338c4c22766e
olavi <olavi@hahtomaa.fi>
2024-07-29 21:09:24 +0300

    Unit

--- src/main/java/org/main/uneton/admin/Wipeprofile.java
+++ src/main/java/org/main/uneton/admin/Wipeprofile.java
@@ -34,7 +34,7 @@ public class Wipeprofile implements CommandExecutor {
         }
 
         if (args.length == 0) {
-            String usage = ColorUtils.colorize("&x&2&C&0&9&1&6&l>&x&5&C&1&2&2&F&l>&x&C&7&5&3&4&7&l> &x&2&E&2&E&2&E&l+ &x&A&B&A&B&A&B/wipeprofile");
+            String usage = ColorUtils.colorize("&x&2&C&0&9&1&6&l>&x&5&C&1&2&2&F&l>&x&C&7&5&3&4&7&l> &x&2&E&2&E&2&E&l+ &x&A&B&A&B&A&B/wipeprofile <player>");
             player.sendMessage(usage);
             return true;
         }
